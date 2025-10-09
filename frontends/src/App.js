1c28d0f36229e28512fe9cb2ed3257ee80a0aebd
muadzbayu <muadz.bayual@gmail.com>
2025-10-09 13:48:25 +0700

    [MOD]

--- frontends/src/App.js
+++ frontends/src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Products TEST kuss</h1>
+      <h1>Prxy</h1>
       <ul>
         {products.map((p, i) => <li key={i}>{p.name}</li>)}
       </ul>
